a95d7694f798e763c1a20faa24e64ce16b38aea3
Tomhead <khungly4@gmail.com>
2021-07-08 20:22:50 +0700

    update

---
+++ src/app/controllers/SiteController.js
@@ -0,0 +1,15 @@
+class SitesController {
+    // GET /home
+    home(req, res) {
+        res.render('home');
+    }
+
+    // GET search
+    search(req, res) {
+        res.render('search');
+    }
+
+
+}
+
+module.exports = new SitesController;
