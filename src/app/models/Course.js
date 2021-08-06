d756423eb618dcbe0bc318e4ceb0133fd00c03de
Tomhead <khungly4@gmail.com>
2021-08-06 19:43:52 +0700

    06/08/2021

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, default: '' },
-    mieuta: { type: String, default: '' },
-    createdAt: { type: Date, default: Date.now },
-    updateAt: { type: Date, default: Date.now },
-});
+const Course = new Schema(
+    {
+        name: { type: String, default: '' },
+        mieuta: { type: String, default: '' },
+        image: { type: String, default: '' },
+        slug: { type: String, slug: 'name', unique: true },
+        videoID: { type: String, default: '' },
+    },
+    {
+        timestamps: true,
+    },
+);
 
 module.exports = mongoose.model('Course', Course);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const { dirname } = require('path');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
@@ -21,6 +22,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'));
+
 // HTTP log
 app.use(morgan('combined'));
 
@@ -29,6 +32,9 @@ app.engine(
     'hbs',
     exphbs({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', 'hbs');
